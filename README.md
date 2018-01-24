f7f8dad6f92a7c34d2e6f4570c69d702093bcceb
gerfra <francescogerratana@gmail.com>
2018-01-24 18:27:38 +0100

    Simple example web scrape with python and BeautifulSoup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PyScrape
-Simple example web scrape with python and BeautifulSoup
+Francesco Gerratana 2018, www.nextechnics.com
+Simple example web scrape with python and BeautifulSoup,
+to see in action the method requests.get,rsplit,os.path.isfile ,urlretrieve etc...
+Run before pip install to install the necessary packages
---
+++ Scrape_Img.py
@@ -0,0 +1,39 @@
+"""
+Francesco Gerratana 2018, www.nextechnics.com
+Simple example web scrape with python and BeautifulSoup,
+to see in action the method requests.get,rsplit,os.path.isfile ,urlretrieve etc...
+Run before pip install to install the necessary packages
+"""
+
+from bs4 import BeautifulSoup
+import requests
+from urllib import urlretrieve
+import os
+import sys
+
+url = "http://www.coverbrowser.com/covers/dreamcast-games"
+ext = "jpg"
+dest = os.path.join(os.getcwd(),"img")
+if not os.path.exists(dest):
+    os.makedirs(dest)
+
+page = requests.get(url).text
+
+soup = BeautifulSoup(page, "html.parser")
+
+data = soup.find_all("p",class_="cover")                                                                                   
+
+for a in data:
+    z = a.find_all("img")[0].get("src")
+    print "Url: http://www.coverbrowser.com"+z+"\n path: "+z.rsplit("/",1)[0]+"\n Name file: "+z.rsplit("/",1)[1]
+    files = os.path.join(dest,z.rsplit("/",1)[1])
+    if os.path.isfile(files):
+        print "---Warning File exists!!!"
+    else:
+        print "Write file to: "+files
+    urlretrieve('http://www.coverbrowser.com'+z,files)
+
+                                              
+                                              
+    
+  
