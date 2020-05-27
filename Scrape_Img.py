d5d7c30549fa2e9c279c3a37e754c91c824ef22e
gerfra <francescogerratana@gmail.com>
2020-05-27 20:19:30 +0200

    Download covers with relative name and run on python 3.8

--- Scrape_Img.py
+++ Scrape_Img.py
@@ -1,14 +1,23 @@
 """
-Francesco Gerratana 2018, www.nextechnics.com
+Francesco Gerratana 2020, www.nextechnics.com
 Simple example web scrape with python and BeautifulSoup,
 to see in action the method requests.get,rsplit,os.path.isfile ,urlretrieve etc...
 Run before pip install to install the necessary packages
 """
 
 from bs4 import BeautifulSoup
+import urllib.request
 import requests
-from urllib import urlretrieve
 import os
+import re
+
+'Remove illegal char windows'
+def cleanpath(dpath):
+    invalid = '<>:"/\|?*'
+    for char in invalid:
+        dpath = dpath.replace(char, ' ')
+    return dpath
+
 
 url = "http://www.coverbrowser.com/covers/dreamcast-games"
 ext = "jpg"
@@ -17,22 +26,15 @@ if not os.path.exists(dest):
     os.makedirs(dest)
 
 page = requests.get(url).text
-
 soup = BeautifulSoup(page, "html.parser")
 
-data = soup.find_all("p",class_="cover")                                                                                   
+data = soup.find_all("p",class_="cover")
+
 
 for a in data:
     z = a.find_all("img")[0].get("src")
-    print "Url: http://www.coverbrowser.com"+z+"\n path: "+z.rsplit("/",1)[0]+"\n Name file: "+z.rsplit("/",1)[1]
-    files = os.path.join(dest,z.rsplit("/",1)[1])
-    if os.path.isfile(files):
-        print "---Warning File exists!!!"
-    else:
-        print "Write file to: "+files
-    urlretrieve('http://www.coverbrowser.com'+z,files)
-
-                                              
-                                              
+    n = a.find_all("img")[0].get("alt")
     
-  
+    files = cleanpath(n)+os.path.splitext(z)[1]
+    imgx = dest+"\\"+files.replace("Dreamcast Games -", "")
+    urllib.request.urlretrieve('http://www.coverbrowser.com'+z,imgx)
